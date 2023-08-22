5b9a62d697df95546ba634c8594fbf13710a3503
MAIRON OSÓRIO LIZ DE SIQUEIRA <mairon_siqueira@estudante.sesisenai.org.br>
2023-08-21 21:14:42 -0300

    feature/add-teacher

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const app = express()
 dotenv.config()
 const port = process.env.PORT
 const professorRouter = require('./routes/professores.js')
+const adicionarProfessorRouter = require('./routes/adicionarProfessor')
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/professores', professorRouter)
+app.use('/professores', adicionarProfessorRouter)
 
 app.listen(port, () => {
     console.log(`Servidor disponivel na porta ${port}`)
