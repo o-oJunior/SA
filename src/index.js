71406f4c4c200cf561fd20568edb8bb83dcdb38b
EUGÊNIO RODRIGUES <eugenio_rodrigues@estudante.sesisenai.org.br>
2023-08-21 21:54:22 -0300

    chore: adding delete teacher functionality

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser')
 const app = express()
 dotenv.config()
 const port = process.env.PORT
+
 const professorRouter = require('./routes/professores.js')
+const deletarProfessorRouter = require('./routes/deleteProfessor')
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/professores', professorRouter)
+app.use('/deletar', deletarProfessorRouter)
 
 app.listen(port, () => {
     console.log(`Servidor disponivel na porta ${port}`)
--- src/model/professores.js
+++ src/model/professores.js
@@ -1,7 +1,7 @@
 function buscarTodosProfessores(database, res) {
     database.query('SELECT * FROM professores').then(
         (resultado) => {
-            res.status(200).send({ produtos: resultado.rows });
+            res.status(200).send({ professores: resultado.rows });
         },
         (erro) => {
             res.status(500).send({ erro: erro });
