f5d885149817119281dac42b63c2aa659a196e05
Edio Mendes Junior <edio.junior@jcatlm.com.br>
2023-08-22 08:09:18 -0300

    fix: resolve teacher

--- src/controller/professores.js
+++ src/controller/professores.js
@@ -1,12 +1,11 @@
-const { buscarTodosProfessores } = require('../model/professores');
-const professoresModel = new ProfessoresModel()
+const { buscarTodosProfessores } = require("../model/professores");
 
-exports.buscarTodosProfessores = (req,res) => {
-    buscarTodosProfessores((error, results) => {
-        if(error){
-            res.status(500).send({error: 'Ocorreu um erro inesperado!'})
-        } else {
-            res.status(200).send(results.rows)
-        }
-    });
-}
+exports.buscarTodosProfessores = (req, res) => {
+  buscarTodosProfessores((error, results) => {
+    if (error) {
+      res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    } else {
+      res.status(200).send(results.rows);
+    }
+  });
+};
--- src/model/professores.js
+++ src/model/professores.js
@@ -1,18 +1,9 @@
-function buscarTodosProfessores(database, res) {
-    database.query('SELECT * FROM professores').then(
-        (resultado) => {
-            res.status(200).send({ professores: resultado.rows });
-        },
-        (erro) => {
-            res.status(500).send({ erro: erro });
-        }
-    );
-const database = require('../config/database');
+const database = require("../config/database");
 
 function buscarTodosProfessores(callback) {
-    database.query('SELECT * FROM professores', callback)
+  database.query("SELECT * FROM professores", callback);
 }
 
 module.exports = {
-    buscarTodosProfessores
-}
+  buscarTodosProfessores,
+};
