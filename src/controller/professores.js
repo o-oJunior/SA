337ffef5338f0d913739364433ca185d9bf379ce
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-08-23 14:34:57 -0300

    chore: adding edit teacher functionality

--- src/controller/professores.js
+++ src/controller/professores.js
@@ -2,6 +2,7 @@ const {
   buscarTodosProfessores,
   deletarProfessor,
   adicionarProfessor,
+  editarProfessor
 } = require("../model/professores");
 
 exports.buscarTodosProfessores = (req, res) => {
@@ -33,7 +34,20 @@ exports.adicionarProfessor = (req, res) => {
     if (error) {
       res.status(500).send({ error: "Ocorreu um erro inesperado!" });
     } else {
-      res.status(201).send({ success: "Professor adicionado com sucesso" });
+      res.status(201).send({ success: "Professor adicionado com sucesso!" });
     }
   });
 };
+
+exports.editarProfessor = (req, res) => {
+  const id = req.params.id;
+  const requisicao = req.body;
+
+  editarProfessor(id, requisicao, (error, results) => {
+    if(error) {
+      res.status(500).send({ error: error });
+    } else {
+      res.status(200).send({ success: "Professor editado com sucesso!" })
+    }
+  });
+}
