f6c1eb3d796105cc1a165d0a06a814e413c722ba
Junior <ediomendesjunior@hotmail.com>
2023-08-25 16:40:56 -0300

    feat: creating edit class in controller

--- src/controller/turma.js
+++ src/controller/turma.js
@@ -1,4 +1,8 @@
-const { buscarTodasTurmas, deletarTurma } = require("../model/turma");
+const {
+  buscarTodasTurmas,
+  deletarTurma,
+  adicionarTurma,
+} = require("../model/turma");
 
 exports.buscarTodasTurmas = (req, res) => {
   buscarTodasTurmas((error, results) => {
@@ -8,16 +12,27 @@ exports.buscarTodasTurmas = (req, res) => {
       res.status(200).send(results.rows);
     }
   });
-}
+};
+
+exports.adicionarTurma = (req, res) => {
+  const turma = req.body;
+  adicionarTurma(turma, (error, results) => {
+    if (error) {
+      res.status(500).send({ erro: "Ocorreu um erro inesperado!" });
+    } else {
+      res.status(200).send({ success: "Turma adicionada com sucesso!" });
+    }
+  });
+};
 
 exports.deletarTurma = (req, res) => {
   const turmaId = req.params.id;
 
   deletarTurma(turmaId, (error, results) => {
     if (error) {
-      res.status(500).send({ error: error });
+      res.status(500).send({ error: "Ocorreu um erro inesperado!" });
     } else {
       res.status(200).send({ success: "Turma removida com sucesso!" });
     }
   });
-}
+};
