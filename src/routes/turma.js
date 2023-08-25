e4186ae773e5f75e462500d544569fd85fbb25da
Junior <ediomendesjunior@hotmail.com>
2023-08-25 16:41:15 -0300

    feat: creating edit class router

--- src/routes/turma.js
+++ src/routes/turma.js
@@ -3,6 +3,7 @@ const router = Router();
 const controller = require("../controller/turma");
 
 router.get("/", controller.buscarTodasTurmas);
+router.get("/adicionar", controller.adicionarTurma)
 router.delete("/deletar/id/:id", controller.deletarTurma);
 
 module.exports = router;
