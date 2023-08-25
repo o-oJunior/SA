519c83767e468bc34c43f53d9aef397484d85455
Junior <ediomendesjunior@hotmail.com>
2023-08-25 19:02:45 -0300

    feat: creating add class router

--- src/routes/turma.js
+++ src/routes/turma.js
@@ -3,6 +3,7 @@ const router = Router();
 const controller = require("../controller/turma");
 
 router.get("/", controller.buscarTodasTurmas);
+router.post("/adicionar", controller.adicionarTurma)
 router.delete("/deletar/id/:id", controller.deletarTurma);
 
 module.exports = router;
