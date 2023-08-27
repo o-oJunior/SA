2f49eb65d214dd77f62419e50e2b52f9f7efc6e1
Junior <ediomendesjunior@hotmail.com>
2023-08-26 23:31:13 -0300

    feat: creating delete discipline router

--- src/routes/disciplina.js
+++ src/routes/disciplina.js
@@ -3,5 +3,6 @@ const router = Router();
 const controller = require("../controller/disciplina");
 
 router.get("/", controller.buscarTodasDisciplinas);
+router.delete("/deletar/id/:id", controller.deletarDisciplina);
 
 module.exports = router;
