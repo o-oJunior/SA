25071c0affdc654422037741eac208e1feaf644a
Junior <ediomendesjunior@hotmail.com>
2023-08-26 23:30:48 -0300

    feat: creating delete discipline in controller

--- src/controller/disciplina.js
+++ src/controller/disciplina.js
@@ -1,4 +1,7 @@
-const { buscarTodasDisciplinas } = require("../model/disciplina");
+const {
+  buscarTodasDisciplinas,
+  deletarDisciplina,
+} = require("../model/disciplina");
 
 exports.buscarTodasDisciplinas = (req, res) => {
   buscarTodasDisciplinas((error, results) => {
@@ -6,6 +9,17 @@ exports.buscarTodasDisciplinas = (req, res) => {
       res.status(500).send({ error: "Ocorreu um erro inesperado!" });
     } else {
       res.status(200).send(results.rows);
+    }
+  });
+};
+
+exports.deletarDisciplina = (req, res) => {
+  const id = req.params.id;
+  deletarDisciplina(id, (error, results) => {
+    if (error) {
+      res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    } else {
+      res.status(200).send({ success: "Disciplina excluida com sucesso!" });
     }
   });
 };
