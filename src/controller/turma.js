80b059b15a30517d8e6634a32403e414c0003eb1
Junior <ediomendesjunior@hotmail.com>
2023-09-07 11:45:22 -0300

    fix: resolving edit and delete class

--- src/controller/turma.js
+++ src/controller/turma.js
@@ -3,14 +3,18 @@ const {
   deletarTurma,
   adicionarTurma,
   editarTurma,
+  buscarTurmaPorID,
 } = require("../model/turma");
+
+const mensagemStatus404 = { error404: "Turma não foi encontrada!" };
+const mensagemStatus500 = { error500: "Ocorreu um erro inesperado!" };
 
 exports.buscarTodasTurmas = async (req, res) => {
   try {
     const response = await buscarTodasTurmas();
     res.status(200).send(response.rows);
   } catch (error) {
-    res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    res.status(500).send(mensagemStatus500);
   }
 };
 
@@ -20,7 +24,7 @@ exports.adicionarTurma = async (req, res) => {
     await adicionarTurma(turma);
     res.status(201).send({ success: "Turma adicionada com sucesso!" });
   } catch (error) {
-    res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    res.status(500).send(mensagemStatus500);
   }
 };
 
@@ -28,19 +32,29 @@ exports.editarTurma = async (req, res) => {
   try {
     const id = req.params.id;
     const turma = req.body;
-    await editarTurma(id, turma);
-    res.status(200).send({ success: "Turma editada com sucesso!" });
+    const buscarTurma = await buscarTurmaPorID(id);
+    if (buscarTurma.rows.length === 0) {
+      res.status(404).send(mensagemStatus404);
+    } else {
+      await editarTurma(id, turma);
+      res.status(200).send({ success: "Turma editada com sucesso!" });
+    }
   } catch (error) {
-    res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    res.status(500).send(mensagemStatus500);
   }
 };
 
 exports.deletarTurma = async (req, res) => {
   try {
-    const turmaId = req.params.id;
-    await deletarTurma(turmaId);
-    res.status(200).send({ success: "Turma removida com sucesso!" });
+    const id = req.params.id;
+    const buscarTurma = await buscarTurmaPorID(id);
+    if (buscarTurma.rows.length === 0) {
+      res.status(404).send(mensagemStatus404);
+    } else {
+      await deletarTurma(turmaId);
+      res.status(200).send({ success: "Turma removida com sucesso!" });
+    }
   } catch (error) {
-    res.status(500).send({ error: "Ocorreu um erro inesperado!" });
+    res.status(500).send(mensagemStatus500);
   }
 };
