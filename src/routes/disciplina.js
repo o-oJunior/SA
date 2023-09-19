8fed504729b854761e69374b024cf02bb8212f5f
o-oJunior <ediomendesjunior@hotmail.com>
2023-09-18 23:08:31 -0300

    refactor: update endpoint discipline

--- src/routes/disciplina.js
+++ src/routes/disciplina.js
@@ -3,16 +3,13 @@ const router = Router();
 const controller = require('../controllers/disciplina');
 
 router.get('/', controller.buscarTodasDisciplinas);
-router.get('/idProfessor=:idProfessor', controller.buscarDisciplinaPorProfessor);
-router.get('/nomeDisciplina=:nomeDisciplina', controller.buscarDisciplinaPorNome);
-router.get('/idTurma=:idTurma', controller.buscarDisciplinaPorTurma);
-router.get('/nome=:nomeDisciplina&idTurma=:idTurma', controller.buscarDisciplinaPorNomeETurma);
+router.get('/professor', controller.buscarDisciplinaPorProfessor);
+router.get('/disciplina', controller.buscarDisciplinaPorNome);
+router.get('/turma', controller.buscarDisciplinaPorTurma);
+router.get('/disciplina&turma', controller.buscarDisciplinaPorNomeETurma);
 router.post('/adicionar', controller.adicionarDisciplina);
-router.delete('/deletar/nome=:nomeDisciplina&idTurma=:idTurma&idDia=:idDia', controller.deletarDiaDisciplina);
-router.delete('/deletar/nome=:nomeDisciplina&idTurma=:idTurma', controller.deletarTurmaDisciplina);
-router.put(
-  '/editar/nome=:nome&semestre=:semestre&cargaHoraria=:carga_horaria&idProfessor=:id_professor&idTurma=:id_turma&idDia=:id_dia',
-  controller.editarDisciplina
-);
+router.delete('/deletar/disciplina&turma&dia', controller.deletarDiaDisciplina);
+router.delete('/deletar/disciplina&turma', controller.deletarTurmaDisciplina);
+router.put('/editar/disciplina', controller.editarDisciplina);
 
 module.exports = router;
