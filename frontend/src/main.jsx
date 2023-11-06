d36944d36802ad466d9e51a97c43c21e15e2f374
MAIRON OSÓRIO LIZ DE SIQUEIRA <mairon_siqueira@estudante.sesisenai.org.br>
2023-11-06 20:00:09 -0300

    improviment/adding-endpoints

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,10 @@ import Home from './pages/home'
 import Ensalamento from './pages/ensalamento'
 import Professores from './pages/professores'
 import Salas from './pages/salas'
+import Turmas from './pages/turmas'
+import CadastrarProfessores from './pages/cadastroProfessores'
+import CadastrarTurmas from './pages/cadastroTurmas'
+import CadastrarSalas from './pages/cadastroSalas'
 
 
 const router = createBrowserRouter([
@@ -18,13 +22,30 @@ const router = createBrowserRouter([
     element: <Professores />
   },
   {
+    path: "/cadastrarProfessores",
+    element: <CadastrarProfessores />
+  },
+  {
     path: "/salas",
     element: <Salas />
   },
   {
+    path: "/cadastrarSalas",
+    element: <CadastrarSalas />
+  },
+  {
+    path: "/turmas",
+    element: <Turmas />
+  },
+  {
+    path: "/cadastrarTurmas",
+    element: <CadastrarTurmas />
+  },
+  {
     path: "/ensalamento",
     element: <Ensalamento />
-  }
+  },
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,23 +1,8 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import Menu from '../components/menu'
+
 export default function Home() {
-  return (
-    <div>
-        <ul>
-        <li><Link to="/professores">
-            <button>Professores</button>
-            </Link></li>
-        <li>
-        <Link to="/salas">
-            <button>Salas</button>
-            </Link>
-        </li>
-        <li>
-        <Link to="/Ensalamento">
-            <button>Ensalamento</button>
-            </Link>
-        </li>
-        </ul>
-    </div>
-  )
+    return (
+        <Menu />
+    )
 }
