951cd39d6c4131cadf3fe80a4f694b2ef24d563b
VINÍCIUS NATANAEL HELLMANN <vinicius_hellmann@estudante.sesisenai.org.br>
2023-11-06 21:19:25 -0300

    feat: create page teacher

--- frontend/src/pages/cadastroProfessores.jsx
+++ frontend/src/pages/cadastroProfessores.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function ListarProfessores() {
+export default function CadastroProfessores() {
   return (
     <div>Cadastro Professores</div>
   )
--- frontend/src/pages/professores.jsx
+++ frontend/src/pages/professores.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Professores() {
+
+  const [professores, setProfessores] = useState('')
+
+  useEffect(() => {
+    buscarProfessores()
+  }, [])
+
+  const buscarProfessores = async () => {
+    try {
+      const response = await fetch('https://api-ensalamento-senai.onrender.com/api/professores')
+      const data = await response.json()
+      setProfessores(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  console.log(professores)
   return (
-    <div>professores</div>
+    <div>
+      <div> Professores </div>
+      <input placeholder='Pesquisar por Nome ou Matricula de Professor' />
+      <Link to='/cadastrarProfessores'><button>Adicionar Professor</button></Link>
+
+    </div>
   )
 }
